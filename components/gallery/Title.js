586bc9ba02f4496ff4ef5aee3920b531511c1ebf
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-11-06 14:03:18 +0300

    Set storage settings for users to upload files

---
+++ components/gallery/Title.js
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <div className="title">
+            <h1>Day Planner Gallery</h1>
+            <h2>All Pictures</h2>
+            <p>One address for uploaded images</p>
+        </div>
+    )
+}
---
+++ pages/gallery/index.js
@@ -0,0 +1,9 @@
+import Title from '../../components/gallery/Title'
+
+export default function Home() {
+    return (
+        <div className="App">
+            <Title />
+        </div>
+    )
+}
