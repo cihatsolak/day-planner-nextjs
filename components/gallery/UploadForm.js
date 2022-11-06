1cc5fe6a1e7935e278648771aca7a1106af6e0d0
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-11-06 14:13:01 +0300

    Create component for file upload

---
+++ components/gallery/UploadForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+export default function UploadForm() {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const typeList = ['image/png', 'image/jpeg'];
+
+    const handleChange = (event) => {
+        let selectedFile = event.target.files[0];
+        if (selectedFile && typeList.includes(selectedFile.type)) {
+            setFile(selectedFile);
+            setError('')
+        } else {
+            setFile(null)
+            setError('Please upload a png or jpeg/jpg file.')
+        }
+
+        console.log(event.target.files);
+    }
+
+    return (
+        <form>
+            <label>
+                <input type="file" onChange={handleChange} />
+                <span>+</span>
+            </label>
+
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
+        </form>
+    )
+}
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,9 +1,11 @@
 import Title from '../../components/gallery/Title'
+import UploadForm from '../../components/gallery/UploadForm'
 
 export default function Home() {
     return (
         <div className="App">
             <Title />
+            <UploadForm />
         </div>
     )
 }
